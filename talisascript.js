7d3c1becf23a8002deb0b7b15b1b8810bb9e50d6
tallyvanv <talisa.vdv@gmail.com>
2020-01-15 14:52:53 +0100

    two fetches triggered on click

--- talisascript.js
+++ talisascript.js
@@ -1,12 +1,18 @@
 (() => {
-    let pokemonInput = document.getElementById("pokemonToGet").value;
-    async function getPokemon() {
-        let path = `https://pokeapi.co/api/v2/pokemon/${pokemonInput}`;
-        const response = await axios.get(path);
-        //const data = await pokedex.json();
-        console.log(response);
-    }
     document.getElementById("run").addEventListener("click", function() {
-        getPokemon()
-    })
+    let pokemonInput = document.getElementById("pokemonToGet").value;
+        async function getPokemon() {
+            let path = `https://pokeapi.co/api/v2/pokemon/${pokemonInput}`;
+            const data = await axios.get(path);
+            //const data = await pokedex.json();
+            console.log(data);
+        }
+        async function getEvolution() {
+            let path = `https://pokeapi.co/api/v2/pokemon-species/${pokemonInput}`;
+            const data = await axios.get(path);
+            console.log(data);
+        }
+        getPokemon();
+        getEvolution();
+    });
 })();
