4abb17f22461637c1206addc00be947587cc365d
tallyvanv <talisa.vdv@gmail.com>
2020-01-15 15:12:21 +0100

    divs for dom testing

--- talisascript.js
+++ talisascript.js
@@ -6,6 +6,12 @@
             const data = await axios.get(path);
             //const data = await pokedex.json();
             console.log(data);
+
+            let pokeId = document.getElementById("pokeId");
+            let sprite = document.getElementById("sprite");
+            let moves = document.getElementById("moves");
+
+
         }
         async function getEvolution() {
             let path = `https://pokeapi.co/api/v2/pokemon-species/${pokemonInput}`;
