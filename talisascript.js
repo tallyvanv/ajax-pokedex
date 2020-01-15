2025cf8825a05ebd262a86730e5291ae87fc2d24
tallyvanv <talisa.vdv@gmail.com>
2020-01-15 12:20:56 +0100

    console logged data

---
+++ talisascript.js
@@ -0,0 +1,10 @@
+(() => {
+    let pokemonInput = document.getElementById("pokemonToGet").value;
+    async function getPokemon() {
+        let path = `https://pokeapi.co/api/v2/pokemon/${pokemonInput}`;
+        const response = await axios.get(path);
+        //const data = await pokedex.json();
+        console.log(response);
+    }
+    document.getElementById("run").onclick = function() {getPokemon()};
+})();
