b470bdb1d0ee19a3f9e1e3bb362cd313c4f30bb7
tallyvanv <talisa.vdv@gmail.com>
2020-01-16 09:34:18 +0100

    add test css

--- talisascript.js
+++ talisascript.js
@@ -4,8 +4,6 @@
         async function getPokemon() {
             let path = `https://pokeapi.co/api/v2/pokemon/${pokemonInput}`;
             const data = await axios.get(path);
-            //const data = await pokedex.json();
-            console.log(data);
 
             let pokeName = document.getElementById("pokeName");
             let pokeId = document.getElementById("pokeId");
@@ -14,18 +12,14 @@
             let moves = document.getElementById("moves");
 
             pokeName.innerHTML = data.data.species.name;
-            console.log(data.data.species.name);
             pokeId.innerHTML = data.data.id;
-            console.log(data.data.id);
             att.value = data.data.sprites.front_default;
-            console.log(att.value);
             sprite.setAttributeNode(att);
 
         }
         async function getEvolution() {
             let path = `https://pokeapi.co/api/v2/pokemon-species/${pokemonInput}`;
             const data = await axios.get(path);
-            console.log(data);
         }
         getPokemon();
         getEvolution();
